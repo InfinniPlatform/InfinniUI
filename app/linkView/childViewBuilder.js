2f157aba52c22dd7a09e127fb037913375d22be3
m.molodorich <m.molodorich@infinnitysolutions.com>
2015-07-21 11:18:41 +0500

    UI-1151: View #progress
    ChildView

--- app/linkView/childViewBuilder.js
+++ app/linkView/childViewBuilder.js
@@ -1,10 +1,10 @@
 function ChildViewBuilder() {
-    this.build = function (context, parent, metadata) {
-        var linkView = parent.getChildView(metadata.Name);
-        linkView.setOpenMode(metadata.OpenMode);
-        linkView.setContainer(metadata.Container);
-        if (['Application', 'Page', 'Dialog'].indexOf(metadata.OpenMode) > -1) {
-            InfinniUI.views.appendView(null, metadata, linkView);
+    this.build = function (context, args) {
+        var linkView = args.parent.getChildView(args.metadata.Name);
+        linkView.setOpenMode(args.metadata.OpenMode);
+        linkView.setContainer(args.metadata.Container);
+        if (['Application', 'Page', 'Dialog'].indexOf(args.metadata.OpenMode) > -1) {
+            InfinniUI.views.appendView(null, args.metadata, linkView);
         }
 
         return linkView;
