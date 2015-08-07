821a9ffd412984e29eca59536a4daf2ce0f43eaa
puritania <kostya.vlasov@gmail.com>
2015-08-07 17:00:14 +0500

    ~wip

--- app/new/controls/listBox/listBoxView.js
+++ app/new/controls/listBox/listBoxView.js
@@ -10,7 +10,13 @@ var ListBoxView = ControlView.extend({
         var items = this.model.get('items');
         this.$items = [];
 
+        //@TODO Вынести в ContainerView??
         items.onAdd(this.onAddItem.bind(this));
+        items.onRemove(this.onRemoveItem.bind(this));
+        items.onReplace(this.onReplaceItem.bind(this));
+        items.onMove(this.onMoveItem.bind(this));
+        items.onReset(this.onResetItem.bind(this));
+
         items.onChange(function (context, argument) {
             console.log('onChange', argument);
         });
@@ -57,7 +63,7 @@ var ListBoxView = ControlView.extend({
         var itemsCollection = model.get('items');
         var itemTemplate = model.get('itemTemplate');
 
-        var elements = newItems.map(function (item) {
+        var $items = newItems.map(function (item) {
             return itemTemplate(undefined, {
                 item: item,
                 index: itemsCollection.indexOf(item)
@@ -67,10 +73,57 @@ var ListBoxView = ControlView.extend({
         if (newStartingIndex === -1) {
             //@TODO Добавить представления элементов в конец
             Array.prototype.push.apply(this.$items, newItems);
-            this.ui.items.append(elements);
+            this.ui.items.append($items);
         } else {
             //@TODO Добавить представления элементов в указанную позицию
+            Array.prototype.splice.apply(this.$items, [newStartingIndex, 0].concat($items));
+            if (newStartingIndex === 0) {
+                this.ui.items.prepend($items);
+            } else {
+                this.$items[newStartingIndex - 1].after($items);
+            }
         }
+    },
+
+    onRemoveItem: function (context, argument) {
+        var
+            oldStartingIndex = argument.oldStartingIndex,
+            oldItems = argument.oldItems;
+
+        if (!this.wasRendered) {
+            return;
+        }
+
+        this.$items
+            .splice(oldStartingIndex, oldItems.length)
+            .forEach(function ($item) {
+                $item.remove();
+            });
+    },
+
+    onReplaceItem: function (context, argument) {
+        //@TODO Полностью перерисовать элементы т.к. недостаточно данных что на что поменялось
+
+    },
+
+    onMoveItem: function (context, argument) {
+        var
+            oldItems = argument.oldItems,
+            oldStartingIndex = argument.oldStartingIndex,
+            newStartingIndex = argument.newStartingIndex;
+
+        if (!this.wasRendered) {
+            return;
+        }
+
+        var $items = this.$items.splice(oldStartingIndex, oldItems.length);
+        Array.prototype.splice.apply(this.$items, [newStartingIndex, 0].concat($items));
+        //#TODO Перенести DOM элементы на новые места
+    },
+
+    onResetItem: function (context, argument) {
+        //@TODO Перерисовать всю коллекцию элементов
+
     }
 
 });
