65caf6500c2c3dae669b5c86f03d628294f5e049
k.vlasov <kostya.vlasov@gmail.com>
2015-09-04 15:45:07 +0500

    ~fix Поправил кодировку

--- app/element/childrenElementCollection.js
+++ app/element/childrenElementCollection.js
@@ -1,45 +1,52 @@
+/**
+ *
+ * @param {Element} element
+ * @constructor
+ */
 function ChildrenElementCollection (element) {
     this._element = element;
     this._parentElement = null;
     this._list = [];
 }
 
-ChildrenElementCollection.prototype.getList = function () {
+/**
+ * @description Возвращает список дочерних элементов
+ * @returns {Array.<Element>}
+ */
+ChildrenElementCollection.prototype.getChildElements = function () {
     return this._list;
 };
 
-ChildrenElementCollection.prototype.setParent = function (parent) {
-    this._parentElement = parent;
-};
-
+/**
+ * @description Возвращает родительский элемент
+ * @returns {null|Element}
+ */
 ChildrenElementCollection.prototype.getParent = function () {
     return this._parentElement;
 };
 
-ChildrenElementCollection.prototype.add = function (element) {
-    this._list.push(element);
-    element.children.setParent(this._element);
-};
-
-ChildrenElementCollection.prototype.clear = function () {
-    var el;
-
-    while(el = this._list.pop()) {
-        el.children
-            .clear()
-            .setParent(null);
-    }
-    return this;
-};
-
+/**
+ * @description Количество дочерних элементов
+ * @returns {Number}
+ */
 ChildrenElementCollection.prototype.length = function () {
     return this._list.length;
 };
 
+/**
+ * @description Возвращает дочерний элемент по указанному индексу
+ * @param index {Number}
+ * @returns {Element|undefined}
+ */
 ChildrenElementCollection.prototype.get = function (index) {
     return this._list[index];
 };
 
+/**
+ * @description Возвращает первый из всех дочерних элементов с заданным именем
+ * @param name {String} Имя элемента @see {@link Element#Name}
+ * @returns {Element|null}
+ */
 ChildrenElementCollection.prototype.findChildrenByName = function (name) {
     var
         elements, element = null;
@@ -59,6 +66,11 @@ ChildrenElementCollection.prototype.findChildrenByName = function (name) {
     return element;
 };
 
+/**
+ * @description Возвращает все дочерние элементы с указанным именем
+ * @param name {String} Имя элемента @see {@link Element#Name}
+ * @returns {Array.<Element>}
+ */
 ChildrenElementCollection.prototype.findAllChildrenByName = function (name) {
     var
         elements = [];
@@ -73,11 +85,16 @@ ChildrenElementCollection.prototype.findAllChildrenByName = function (name) {
     return elements;
 };
 
+/**
+ * @description Возвращает родительский элемент с заданным именем
+ * @param name {String} Имя элемента @see {@link Element#Name}
+ * @returns {Element|null}
+ */
 ChildrenElementCollection.prototype.findParentByName = function (name) {
     var
         parentElement,
         children = this,
-        element;
+        element = null;
 
 
     while (parentElement = children && children.getParent()) {
@@ -93,4 +110,24 @@ ChildrenElementCollection.prototype.findParentByName = function (name) {
     }
 
     return element;
+};
+
+ChildrenElementCollection.prototype.setParent = function (parent) {
+    this._parentElement = parent;
+};
+
+ChildrenElementCollection.prototype.add = function (element) {
+    this._list.push(element);
+    element.children.setParent(this._element);
+};
+
+ChildrenElementCollection.prototype.clear = function () {
+    var el;
+
+    while(el = this._list.pop()) {
+        el.children
+            .clear()
+            .setParent(null);
+    }
+    return this;
 };
