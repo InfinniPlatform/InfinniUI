4a4301e0dc8116bdc013abf537626e942b97f4e6
k.maksimov <k.maksimov@infinnitysolutions.com>
2015-09-09 10:32:34 +0500

    fix tests

--- app/new/controls/_base/container/containerModel.js
+++ app/new/controls/_base/container/containerModel.js
@@ -7,12 +7,13 @@ var ContainerModel = ControlModel.extend(
     /** @lends ContainerModel.prototype */
     {
         defaults: _.defaults({
-            items: new Collection(),
+            //items: new Collection()
             itemTemplate: null
         }, ControlModel.prototype.defaults),
 
         initialize: function () {
             ControlModel.prototype.initialize.apply(this, Array.prototype.slice.call(arguments));
+            this.set('items', new Collection());
         }
     }
 );
