8569d7f67c2920f9873e3ffe9434c2cc12da79e3
k.maksimov <k.maksimov@infinnitysolutions.com>
2015-09-10 12:40:53 +0500

    toggle button properties

--- app/new/controls/toggleButton/toggleButtonModel.js
+++ app/new/controls/toggleButton/toggleButtonModel.js
@@ -1,5 +1,12 @@
 var ToggleButtonModel = ControlModel.extend( _.extend({
 
+    defaults: _.defaults({
+        value: true,
+        textOn: 'ON',
+        textOff: 'OFF',
+        horizontalAlignment: 'Left'
+    }),
+
     initialize: function () {
         ControlModel.prototype.initialize.apply(this, arguments);
         this.initialize_editorBaseModel();
