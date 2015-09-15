6d538331ddd7b01fd4dd0390b8175a45b201ed05
k.maksimov <k.maksimov@infinnitysolutions.com>
2015-09-15 13:52:14 +0500

    viewView

---
+++ app/new/controls/_base/container/containerView.js
@@ -0,0 +1,59 @@
+/**
+ * @class
+ * @augments ControlView
+ */
+var ViewView = ControlView.extend(
+    /** @lends StackPanelView.prototype */
+    {
+        className: 'pl-view',
+
+        initialize: function (options) {
+            ControlView.prototype.initialize.call(this, options);
+
+            var that = this;
+            this.model.get('items').onChange(function(){
+                that.rerender();
+            });
+            this.initOrientation();
+        },
+
+        render: function () {
+            this.prerenderingActions();
+
+            this.$el.empty();
+            this.$el.html(this.template({
+                items: this.model.get('items')
+            }));
+            this.renderItemsContents();
+
+            this.bindUIElements();
+
+            this.postrenderingActions();
+            return this;
+        },
+
+        renderItemsContents: function(){
+            var $items = this.$el.find('.pl-stack-panel-i'),
+                items = this.model.get('items'),
+                itemTemplate = this.model.get('itemTemplate'),
+                element, item;
+
+            $items.each(function(i, el){
+                item = items.getByIndex(i);
+                element = itemTemplate(undefined, {item: item, index: i});
+                $(el)
+                    .append(element.render());
+            });
+        },
+
+        initOrientation: function () {
+            this.listenTo(this.model, 'change:orientation', this.updateOrientation);
+            this.updateOrientation();
+        },
+
+        updateOrientation: function () {
+            var orientation = this.model.get('orientation');
+            this.$el.toggleClass('horizontal-orientation', orientation == 'Horizontal');
+        }
+    }
+);
