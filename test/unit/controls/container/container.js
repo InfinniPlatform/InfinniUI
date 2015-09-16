17df3d79bd59478a846cb1dfe3b947fd5ce91d83
xostik <xostiki@ya.ru>
2015-09-16 23:46:46 +0500

    tests for container (#progress)

--- test/unit/controls/container/container.js
+++ test/unit/controls/container/container.js
@@ -15,7 +15,7 @@ describe('Container (Control)', function () {
     }
 
     describe('StackPanel as exemplar of Container', function () {
-        var metadata2 = {
+        var metadata1 = {
             Text: 'Пациенты',
             DataSources : [
                 {
@@ -53,296 +53,210 @@ describe('Container (Control)', function () {
                 }
             }]
         };
-        it('should render stackPanel', function () {
+
+        it('should render stackPanel with templating items', function () {
             // Given When
-            var linkView = new LinkView(null, function (resultCallback) {
-                var builder = new ApplicationBuilder();
-                var view = builder.buildType('View', metadata2, {parentView: fakeView()});
-                resultCallback(view);
-            });
-            linkView.setOpenMode('Application');
-
-            var view = linkView.createView(function (view) {
-                view.open();
-
-                var $stackPanel = $('#sandbox').children();
-                $stackPanel.detach();
-
-                onListboxReady($stackPanel);
-            });
+            var metadata = metadata1;
+            applyViewMetadata(metadata, onViewReady);
 
             // Then
-            function onListboxReady($stackPanel){
-                assert.lengthOf($stackPanel.find('.pl-stack-panel-i'), 3, 'length of rendered stackPanel');
-                assert.lengthOf($stackPanel.find('.pl-text-box-input'), 3, 'length of rendered textbox');
-                assert.equal($stackPanel.find('.pl-text-box-input:first').val(), 'LTE', 'binding in itemTemplate is right');
+            function onViewReady(view, $layout){
+                $layout.detach();
+
+                assert.lengthOf($layout.find('.pl-stack-panel-i'), 3, 'length of rendered stackPanel');
+                assert.lengthOf($layout.find('.pl-text-box-input'), 3, 'length of rendered textbox');
+                assert.equal($layout.find('.pl-text-box-input:first').val(), 'LTE', 'binding in itemTemplate is right');
             }
         });
-    });
 
 
+        var metadata2 = {
+            Text: 'Пациенты',
+            DataSources : [
+                {
+                    ObjectDataSource: {
+                        "Name": "ObjectDataSource1",
+                        "Items": [
+                            { "Id": 1, "Display": "LTE" },
+                            { "Id": 2, "Display": "3G" },
+                            { "Id": 3, "Display": "2G" }
+                        ]
+                    }
+                }
+            ],
+            Items: [{
 
-    describe('render', function () {
-        it('should render stackPanel', function () {
-            // Given When
-            var linkView = new LinkView(null, function (resultCallback) {
-                var builder = new ApplicationBuilder();
-                var view = builder.buildType('View', metadata2, {parentView: fakeView()});
-                resultCallback(view);
-            });
-            linkView.setOpenMode('Application');
-
-            var view = linkView.createView(function (view) {
-                view.open();
-
-                var $stackPanel = $('#sandbox').children();
-                $stackPanel.detach();
-
-                onListboxReady($stackPanel);
-            });
-
-            // Then
-            function onListboxReady($stackPanel){
-                assert.lengthOf($stackPanel.find('.pl-stack-panel-i'), 3, 'length of rendered stackPanel');
-                assert.lengthOf($stackPanel.find('.pl-text-box-input'), 3, 'length of rendered textbox');
-                assert.equal($stackPanel.find('.pl-text-box-input:first').val(), 'LTE', 'binding in itemTemplate is right');
-            }
-        });
-    });
-
-
-    var metadata3 = {
-        Text: 'Пациенты',
-        DataSources : [
-            {
-                ObjectDataSource: {
-                    "Name": "ObjectDataSource1",
-                    "Items": [
-                        { "Id": 1, "Display": "LTE" },
-                        { "Id": 2, "Display": "3G" },
-                        { "Id": 3, "Display": "2G" }
+                StackPanel: {
+                    Name: 'MainViewPanel',
+                    "Items" : [
+                        {
+                            "TextBox": {
+                                "Name": "TextBox1",
+                                "Value": {
+                                    "PropertyBinding":{
+                                        "Source": "ObjectDataSource1",
+                                        "Property": "Display"
+                                    }
+                                }
+                            }
+                        },{
+                            "TextBox": {
+                                "Name": "TextBox2",
+                                "Value": {
+                                    "PropertyBinding":{
+                                        "Source": "ObjectDataSource1",
+                                        "Property": "Id"
+                                    }
+                                }
+                            }
+                        }
                     ]
                 }
-            }
-        ],
-        Items: [{
+            }]
+        };
 
-            StackPanel: {
-                Name: 'MainViewPanel',
-                "Items" : [
-                    {
+        it('should render stackPanel with not templating items', function () {
+            // Given When
+            var metadata = metadata2;
+            applyViewMetadata(metadata, onViewReady);
+
+            // Then
+            function onViewReady(view, $layout){
+                $layout.detach();
+
+                assert.lengthOf($layout.find('.pl-stack-panel-i'), 2, 'length of rendered stackPanel');
+                assert.lengthOf($layout.find('.pl-text-box-input'), 2, 'length of rendered textbox');
+                assert.equal($layout.find('.pl-text-box-input:first').val(), 'LTE', 'binding in itemTemplate is right');
+            }
+        });
+    });
+
+
+
+    describe('ListBox as exemplar of Container', function (){
+        var metadata1 = {
+            Text: 'Пациенты',
+            DataSources : [
+                {
+                    ObjectDataSource: {
+                        "Name": "ObjectDataSource1",
+                        "Items": [
+                            { "Id": 1, "Display": "LTE" },
+                            { "Id": 2, "Display": "2G" },
+                            { "Id": 3, "Display": "2G" }
+                        ]
+                    }
+                }
+            ],
+            Items: [{
+
+                StackPanel: {
+                    Name: 'MainViewPanel',
+                    "Items" : [
+                        {
+                            ListBox: {
+                                "ItemTemplate": {
+                                    "TextBox": {
+                                        "Name": "TextBox1",
+                                        "Value": {
+                                            "PropertyBinding":{
+                                                "Source": "ObjectDataSource1",
+                                                "Property": "$.Display"
+                                            }
+                                        }
+                                    }
+                                },
+                                "GroupItemTemplate": {
+                                    "TextBox": {
+                                        "Value": {
+                                            "PropertyBinding":{
+                                                "Source": "ObjectDataSource1",
+                                                "Property": "$.Display"
+                                            }
+                                        }
+                                    }
+                                },
+                                "GroupValueProperty": "Display",
+                                "Items" : {
+                                    "PropertyBinding": {
+                                        "Source": "ObjectDataSource1",
+                                        "Property": ""
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                }
+            }]
+        };
+
+        it('should render listBox with grouping', function () {
+            // Given When
+            var metadata = metadata1;
+            applyViewMetadata(metadata, onViewReady);
+
+            // Then
+            function onViewReady(view, $layout){
+                $layout.detach();
+
+                assert.lengthOf($layout.find('.pl-listbox-body'), 3, 'length of rendered listbox');
+            }
+        });
+
+
+        var metadata2 = {
+            Text: 'Пациенты',
+            DataSources : [
+                {
+                    ObjectDataSource: {
+                        "Name": "ObjectDataSource1",
+                        "Items": [
+                            { "Id": 1, "Display": "LTE" },
+                            { "Id": 2, "Display": "2G" },
+                            { "Id": 3, "Display": "2G" }
+                        ]
+                    }
+                }
+            ],
+            Items: [{
+
+                ListBox: {
+                    "ItemTemplate": {
                         "TextBox": {
                             "Name": "TextBox1",
                             "Value": {
                                 "PropertyBinding":{
                                     "Source": "ObjectDataSource1",
-                                    "Property": "Display"
+                                    "Property": "$.Display"
                                 }
                             }
                         }
-                    },{
-                        "TextBox": {
-                            "Name": "TextBox2",
-                            "Value": {
-                                "PropertyBinding":{
-                                    "Source": "ObjectDataSource1",
-                                    "Property": "Id"
-                                }
-                            }
+                    },
+                    "Items" : {
+                        "PropertyBinding": {
+                            "Source": "ObjectDataSource1",
+                            "Property": ""
                         }
-                    }
-                ]
-            }
-        }]
-    };
-
-    describe('render2', function () {
-        it('should render stackPanel', function () {
-            // Given When
-            window.providerRegister.register('DocumentDataSource', function () {
-                return new FakeDataProvider();
-            });
-
-            var linkView = new LinkView(null, function (resultCallback) {
-                var builder = new ApplicationBuilder();
-                var view = builder.buildType('View', metadata3, {parentView: fakeView()});
-                resultCallback(view);
-            });
-            linkView.setOpenMode('Application');
-
-            var view = linkView.createView(function (view) {
-                view.open();
-
-                var $stackPanel = $('#sandbox').children();
-                $stackPanel.detach();
-
-                onListboxReady($stackPanel);
-            });
-
-            // Then
-            function onListboxReady($stackPanel){
-                assert.lengthOf($stackPanel.find('.pl-stack-panel-i'), 2, 'length of rendered stackPanel');
-                assert.lengthOf($stackPanel.find('.pl-text-box-input'), 2, 'length of rendered textbox');
-                assert.equal($stackPanel.find('.pl-text-box-input:first').val(), 'LTE', 'binding in itemTemplate is right');
-            }
-        });
-    });
-
-
-    var metadata = {
-        Text: 'Пациенты',
-        DataSources : [
-            {
-                ObjectDataSource: {
-                    "Name": "ObjectDataSource1",
-                    "Items": [
-                        { "Id": 1, "Display": "LTE" },
-                        { "Id": 2, "Display": "2G" },
-                        { "Id": 3, "Display": "2G" }
-                    ]
-                }
-            }
-        ],
-        Items: [{
-
-            StackPanel: {
-                Name: 'MainViewPanel',
-                "Items" : [
-                    {
-                        ListBox: {
-                            "ItemTemplate": {
-                                "TextBox": {
-                                    "Name": "TextBox1",
-                                    "Value": {
-                                        "PropertyBinding":{
-                                            "Source": "ObjectDataSource1",
-                                            "Property": "$.Display"
-                                        }
-                                    }
-                                }
-                            },
-                            "GroupItemTemplate": {
-                                "TextBox": {
-                                    "Value": {
-                                        "PropertyBinding":{
-                                            "Source": "ObjectDataSource1",
-                                            "Property": "$.Display"
-                                        }
-                                    }
-                                }
-                            },
-                            "GroupValueProperty": "Display",
-                            "Items" : {
-                                "PropertyBinding": {
-                                    "Source": "ObjectDataSource1",
-                                    "Property": ""
-                                }
-                            }
-                        }
-                    }
-                ]
-            }
-        }]
-    };
-
-    describe('render lb', function () {
-        it('should render listBox', function () {
-            // Given When
-            window.providerRegister.register('DocumentDataSource', function () {
-                return new FakeDataProvider();
-            });
-
-            var linkView = new LinkView(null, function (resultCallback) {
-                var builder = new ApplicationBuilder();
-                var view = builder.buildType('View', metadata, {parentView: fakeView()});
-                resultCallback(view);
-            });
-            linkView.setOpenMode('Application');
-
-            var view = linkView.createView(function (view) {
-                view.open();
-
-                var $listbox = $('#sandbox').children();
-                $listbox.detach();
-
-                onListboxReady($listbox);
-            });
-
-            // Then
-            function onListboxReady($listbox){
-                assert.lengthOf($listbox.find('.pl-listbox-body'), 3, 'length of rendered listbox');
-            }
-        });
-    });
-
-
-    var metadata4 = {
-        Text: 'Пациенты',
-        DataSources : [
-            {
-                ObjectDataSource: {
-                    "Name": "ObjectDataSource1",
-                    "Items": [
-                        { "Id": 1, "Display": "LTE" },
-                        { "Id": 2, "Display": "2G" },
-                        { "Id": 3, "Display": "2G" }
-                    ]
-                }
-            }
-        ],
-        Items: [{
-
-            ListBox: {
-                "ItemTemplate": {
-                    "TextBox": {
-                        "Name": "TextBox1",
-                        "Value": {
-                            "PropertyBinding":{
-                                "Source": "ObjectDataSource1",
-                                "Property": "$.Display"
-                            }
-                        }
-                    }
-                },
-                "Items" : {
-                    "PropertyBinding": {
-                        "Source": "ObjectDataSource1",
-                        "Property": ""
                     }
                 }
-            }
-        }]
-    };
+            }]
+        };
 
-    describe('render lb2', function () {
         it('should render listBox without grouping', function () {
             // Given When
-            window.providerRegister.register('DocumentDataSource', function () {
-                return new FakeDataProvider();
-            });
-
-            var linkView = new LinkView(null, function (resultCallback) {
-                var builder = new ApplicationBuilder();
-                var view = builder.buildType('View', metadata4, {parentView: fakeView()});
-                resultCallback(view);
-            });
-            linkView.setOpenMode('Application');
-
-            var view = linkView.createView(function (view) {
-                view.open();
-
-                var $listbox = $('#sandbox').children();
-                $listbox.detach();
-
-                onListboxReady($listbox);
-            });
+            var metadata = metadata2;
+            applyViewMetadata(metadata, onViewReady);
 
             // Then
-            function onListboxReady($listbox){
-                assert.lengthOf($listbox.find('.pl-listbox-body'), 3, 'length of rendered listbox');
+            function onViewReady(view, $layout){
+                $layout.detach();
+
+                assert.lengthOf($layout.find('.pl-listbox-body'), 3, 'length of rendered listbox');
             }
         });
     });
+
+
 
 
 
