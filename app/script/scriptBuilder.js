7e1c2dfa625bfe242a367ddc94cf1e1d81060451
k.vlasov <kostya.vlasov@gmail.com>
2015-10-01 11:59:27 +0500

    UI-1294: Избавиться от Script. Заменить на function(context, argument)#resolve

--- app/script/scriptBuilder.js
+++ app/script/scriptBuilder.js
@@ -2,11 +2,27 @@
  *
  * @constructor
  */
-function ScriptBuilder() {}
+function ScriptBuilder() {
+}
 
-_.extend( ScriptBuilder.prototype, {
-    build: function (context, args) {
-        return new Script(args.metadata.Body, args.metadata.Name);
-    }
-});
+ScriptBuilder.prototype.build = function (context, args) {
+    var
+        metadata = args.metadata,
+        name = metadata.Name,
+        body = metadata.Body;
+
+    var func = new Function('context', 'args', body);
+
+    return function (context, args) {
+        var result;
+        try {
+            result = func.call(undefined, context, args);
+        } catch (err) {
+            console.groupCollapsed('%2$s: %1$c%3$s', 'color: #ff0000', name, err.message);
+            console.error(body);
+            console.groupEnd();
+        }
+        return result;
+    };
+};
 
--- app/script/scriptExecutor.js
+++ app/script/scriptExecutor.js
@@ -1,16 +1,28 @@
+/**
+ *
+ * @param parent
+ * @constructor
+ */
 function ScriptExecutor(parent) {
-
-    this.executeScript = function (scriptName, args) {
-
-        var context = parent.getContext();
-        var result;
-
-        var scriptCompiled = parent.getScripts().getById(scriptName);
-
-        if (context && scriptCompiled) {
-            result = scriptCompiled.func.run(context, args);
-        }
-
-        return result;
-    }
+    this.parent = parent;
 }
+
+/**
+ *
+ * @param {string} scriptName
+ * @param {Object} args
+ * @returns {*}
+ */
+ScriptExecutor.prototype.executeScript = function (scriptName, args) {
+    var parent = this.parent;
+    var context = parent.getContext();
+    var result;
+
+    var scriptCompiled = parent.getScripts().getById(scriptName);
+
+    if (context && scriptCompiled) {
+        result = scriptCompiled.func.call(undefined, context, args);
+    }
+
+    return result;
+};
