03f2170b183e8bf76565ccadb463f26771593ef6
s.kochkin <s.kochkin@infinnitysolutions.com>
2015-10-08 14:55:33 +0500

    fix от прикладников

--- app/config.js
+++ app/config.js
@@ -3,5 +3,5 @@ InfinniUI.config.maxLengthUrl = 2048;
 InfinniUI.config.cacheMetadata = false; //boolean - enable/disable cache | milliseconds
 InfinniUI.config.serverUrl = 'http://localhost:9900';
 InfinniUI.config.configId = 'PTA';
-InfinniUI.config.configName = 'Родительский комитет';
+InfinniUI.config.configName = 'Хабинет';
 InfinniUI.config.useLoaderIndicator = true;
