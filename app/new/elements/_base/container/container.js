d7d834d73ed2c92cb2dafc1bcff2bd6c54c424c8
k.vlasov <kostya.vlasov@gmail.com>
2015-10-26 14:19:25 +0500

    ~fix Перенес set/get методы для группировок из ListEditorBase в Container

--- app/new/elements/_base/container/container.js
+++ app/new/elements/_base/container/container.js
@@ -23,3 +23,28 @@ Container.prototype.setItemTemplate = function (itemTemplate) {
 Container.prototype.getItems = function () {
     return this.control.get('items');
 };
+
+Container.prototype.getGroupValueSelector = function () {
+    return this.control.get('groupValueSelector');
+};
+
+Container.prototype.setGroupValueSelector = function (value) {
+    this.control.set('groupValueSelector', value);
+};
+
+Container.prototype.getGroupItemTemplate = function () {
+    return this.control.get('groupItemTemplate');
+};
+
+Container.prototype.setGroupItemTemplate = function (value) {
+    this.control.set('groupItemTemplate', value);
+};
+
+Container.prototype.getGroupItemComparator = function () {
+    return this.control.get('groupItemComparator');
+};
+
+Container.prototype.setGroupItemComparator = function (value) {
+    this.control.set('groupItemComparator', value);
+};
+
