d0bf70f0d35a451de28846dc014f3dc70a8c8375
m.sadofyev <m.sadofyev@infinnity.ru>
2015-11-05 14:46:29 +0500

    PC-1390: Использовать snapshot перед запуском тестов #resolved

--- test/integration/features/support/hooks.js
+++ test/integration/features/support/hooks.js
@@ -1,20 +1,42 @@
+
 this.BeforeScenario( function(scenario, callback) {
-
-	window.configWindow = window.open(window.IntegrationTestConfig.host);	
+	console.log('Before scenario');
+		//delete all indices, create repository and restore
+		client.indices.delete({
+			"index": "_all",
+			"waitForCompletion": true
+		}).then(function(){
+			client.snapshot.createRepository({
+				"repository": "my_backup",
+				"body":{
+					"type": "fs",
+					"settings": {
+						"compress": "true",
+						"location": "C:\\ESbackups"
+					}
+				}
+			}).then(function(){
+				client.snapshot.restore({
+					"repository": "my_backup",
+					"snapshot": "snapshot1",
+					"waitForCompletion": true
+				}).then(function(){
+					window.configWindow = window.open(window.IntegrationTestConfig.host);	
 	
-	var signOut = function(){
-		window.configWindow.contextApp.context.Global.session.signOut(function () {
-			window.configWindow.location.reload();
-			callback();
-		});	
-	};
-
-	window.testHelpers.waitCondition(function(){
-		return window.configWindow.contextApp != null;
-	}, signOut);
+					var signOut = function(){
+						window.configWindow.contextApp.context.Global.session.signOut(function () {
+							window.configWindow.location.reload();
+							callback();
+						});	
+					};
+				
+					window.testHelpers.waitCondition(function(){
+						return window.configWindow.contextApp != null;
+					}, signOut);
+				});	
+			});
+		});
 });
-
-
 
 this.AfterScenario( function(scenario, callback) {
 	window.configWindow.close();
