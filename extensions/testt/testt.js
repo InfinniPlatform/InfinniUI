bfe46dafe8996493b29080a1b5819408a97ebcb1
k.maksimov <k.maksimov@infinnitysolutions.com>
2015-11-09 16:12:55 +0500

    extensionPanel

---
+++ extensions/testt/testt.js
@@ -0,0 +1,14 @@
+function Testt(context, args) {
+    this.context = args.context;
+
+    this.$el = args.$el;
+    this.parameters = args.parameters;
+    this.itemTemplate = args.itemTemplate;
+}
+
+_.extend( Testt.prototype, {
+    render: function(){
+        this.$el
+            .append(this.itemTemplate(this.context, {index:0}).render());
+    }
+});
