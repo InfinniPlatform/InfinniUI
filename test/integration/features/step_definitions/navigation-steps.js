f62f5d1e9b72489e2d71165bdfa69b457d267346
m.sadofyev <m.sadofyev@infinnity.ru>
2015-11-17 16:53:44 +0500

    PC-1967: Добавить конструкцию Если я нажму на ссылку "" #resolved

--- test/integration/features/step_definitions/navigation-steps.js
+++ test/integration/features/step_definitions/navigation-steps.js
@@ -11,14 +11,70 @@ this.When(/^я нажму на кнопку "([^"]*)"$/, function (buttonName, n
 });
 
 this.When(/^я нажму на ссылку "([^"]*)"$/, function (linkName, next) {
-    try {
-		var link = window.currentViewContext.Controls[linkName];
-		chai.assert.isDefined(link);
-        link.control.controlView.$el.click();
-		next();
-    } catch (err) {
-        next(err);
-    }
+	var haveLink = function(){
+		return window.currentViewContext.Controls[linkName] != undefined;
+	}
+	
+	var success = function(){
+		try{
+			var link = window.currentViewContext.Controls[linkName];
+			link.click();
+			next();
+		}catch(err){
+			next(err);
+		}
+	}
+	
+	var fail = function(){
+		next(new Error(linkName + " not found!"));
+	}
+	
+	window.testHelpers.waitCondition(haveLink, success, fail);
+});
+
+this.When(/^я нажму на ссылку "([^"]*)" в списке "([^"]*)"$/, function (linkName, listBoxName, next) {
+	var haveLink = function(){
+		return 	window.currentViewContext.Controls[linkName] != undefined &&
+				window.currentViewContext.Controls[listBoxName] != undefined;
+	}
+	
+	var success = function(){
+		try{
+			var listBox = window.currentViewContext.Controls[listBoxName]; 
+			var link = window.currentViewContext.Controls[linkName];
+			var items = listBox.getItems();
+			
+			if(items.length != 0){
+				var obj = null;
+				
+				items.forEach(function(item){
+					if(item.Name == link.getValue()){
+						obj = item;
+					}
+				});
+				
+				if(obj != null){
+					listBox.setSelectedItem(obj);
+					link.click();
+					next();
+				}else{
+					next(new Error(linkName + " not found!"));
+				}
+				
+			}else{
+				next(new Error("Empty listbox '" + listBoxName + "'"));
+			}
+		}catch(err){
+			next(err);
+		}
+	}
+	
+	var fail = function(){
+		var errorString = (window.currentViewContext.Controls[linkName] == undefined ? linkName : listBoxName) + " not found!";
+		next(new Error(errorString));
+	}
+	
+	window.testHelpers.waitCondition(haveLink, success, fail);
 });
 
 this.When(/^я нажму на выпадающий список кнопок "([^"]*)"$/, function (buttonName, next) {
