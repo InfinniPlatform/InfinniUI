90b88c57a5a5d776202207d34222fdaf5e7f389d
k.maksimov <k.maksimov@infinnitysolutions.com>
2015-11-18 10:37:16 +0500

    remove unusable code

--- app/controls/button/buttonModel.js
+++
@@ -1,8 +0,0 @@
-var ButtonModel = ControlModel.extend({
-    defaults: _.defaults({
-        action: null,
-        horizontalAlignment: 'Left',
-        image: null,
-        parentEnabled: true
-    }, ControlModel.prototype.defaults)
-});
