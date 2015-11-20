59f539e004dd020323f811ce61c72719e820dc59
k.maksimov <k.maksimov@infinnitysolutions.com>
2015-11-20 10:51:26 +0500

    remove unusable code

--- app/data/dataSource/documentDataSourceBuilder.js
+++ app/data/dataSource/documentDataSourceBuilder.js
@@ -1,15 +1,27 @@
-function oldDocumentDataSourceBuilder() {
-
-    this.build = function (context, args){
-
-        var idProperty = args.metadata.IdProperty;
-        if (idProperty == undefined) {
-            idProperty = 'Id';
-        }
-        var dataSource = new DocumentDataSource(args.view, args.metadata);
-        new BaseDataSourceBuilder().build(context,
-                                            _.extend(args, {dataSource: dataSource}));
-
-        return dataSource;
-    }
+function DocumentDataSourceBuilder() {
 }
+
+_.inherit(DocumentDataSourceBuilder, BaseDataSourceBuilder);
+
+_.extend(DocumentDataSourceBuilder.prototype, {
+    applyMetadata: function(builder, parent, metadata, dataSource){
+        BaseDataSourceBuilder.prototype.applyMetadata.call(this, builder, parent, metadata, dataSource);
+        dataSource.setConfigId(metadata.ConfigId);
+        dataSource.setDocumentId(metadata.DocumentId);
+    },
+
+    createDataSource: function(parent){
+        return new DocumentDataSource({
+            view: parent
+        });
+    },
+
+    initFileProvider: function (dataSource) {
+        var fileProvider = window.providerRegister.build('DocumentFileProvider', {
+            documentId: dataSource.getDocumentId(),
+            configId: dataSource.getConfigId()
+        });
+
+        dataSource.setFileProvider(fileProvider);
+    }
+});
