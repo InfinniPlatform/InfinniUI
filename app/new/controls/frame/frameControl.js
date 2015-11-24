3a48af92bfbe87d5361294d57c443a20f936f9a5
k.vlasov <kostya.vlasov@gmail.com>
2015-11-24 18:07:15 +0500

    UI-1386: Frame#progress

---
+++ app/new/controls/frame/frameControl.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * @constructor
+ * @augments Control
+ * @mixes editorBaseControlMixin
+ */
+var FrameControl = function () {
+    _.superClass(FrameControl, this);
+    this.initialize_editorBaseControl();
+};
+
+_.inherit(FrameControl, Control);
+
+_.extend(FrameControl.prototype, {
+
+    createControlModel: function () {
+        return new FrameModel();
+    },
+
+    createControlView: function (model) {
+        return new FrameView({model: model});
+    }
+
+}, editorBaseControlMixin);
