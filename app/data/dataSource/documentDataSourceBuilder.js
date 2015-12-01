084f9fd168ace0fc6e363f5574b26b7ee584f610
k.maksimov <k.maksimov@infinnitysolutions.com>
2015-12-01 10:11:50 +0500

    set custom actions

--- app/data/dataSource/documentDataSourceBuilder.js
+++ app/data/dataSource/documentDataSourceBuilder.js
@@ -6,8 +6,23 @@ _.inherit(DocumentDataSourceBuilder, BaseDataSourceBuilder);
 _.extend(DocumentDataSourceBuilder.prototype, {
     applyMetadata: function(builder, parent, metadata, dataSource){
         BaseDataSourceBuilder.prototype.applyMetadata.call(this, builder, parent, metadata, dataSource);
-        dataSource.setConfigId(metadata.ConfigId);
-        dataSource.setDocumentId(metadata.DocumentId);
+
+        dataSource.setConfigId(metadata['ConfigId']);
+        dataSource.setDocumentId(metadata['DocumentId']);
+
+        if('CreateAction' in metadata){
+            dataSource.setCreateAction(metadata['CreateAction']);
+        }
+        if('ReadAction' in metadata){
+            dataSource.setReadAction(metadata['ReadAction']);
+        }
+        if('UpdateAction' in metadata){
+            dataSource.setUpdateAction(metadata['UpdateAction']);
+        }
+        if('DeleteAction' in metadata){
+            dataSource.setDeleteAction(metadata['DeleteAction']);
+        }
+
     },
 
     createDataSource: function(parent){
