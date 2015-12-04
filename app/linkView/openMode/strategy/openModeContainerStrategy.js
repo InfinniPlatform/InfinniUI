c77bc377bbf388e525449748e7628ded2c3b2531
k.maksimov <k.maksimov@infinnitysolutions.com>
2015-12-04 09:51:19 +0500

    remove unusable code

--- app/linkView/openMode/strategy/openModeContainerStrategy.js
+++ app/linkView/openMode/strategy/openModeContainerStrategy.js
@@ -1,48 +1,4 @@
 var OpenModeContainerStrategy = function () {
-
-    //var openMode = InfinniUI.global.openMode;
-    //
-    //var containerName,
-    //    layoutPanel;
-    //
-    //openMode.resolveContainer([linkView.getContainer(), 'MainContainer'], function (name, layout) {
-    //    containerName = name;
-    //    layoutPanel = layout;
-    //});
-    //
-    //this.open = function (view, $elView) {
-    //
-    //    if (_.isEmpty(layoutPanel)) {//Отобразить в корневом контейнере
-    //        var $rootContainer = openMode.getRootContainer();
-    //        var oldView = $rootContainer.data('view');
-    //        if (typeof oldView !== 'undefined' && oldView !== null) {
-    //            oldView.close();
-    //        }
-    //        $rootContainer.empty();
-    //        $rootContainer.append($elView);
-    //        $rootContainer.data('view', view);
-    //    } else {//Есть элемент с указанным именем
-    //        window.InfinniUI.global.messageBus
-    //            .send(messageTypes.onViewOpened, {
-    //                source: linkView,
-    //                view: view,
-    //                $view: $elView,
-    //                container: containerName,
-    //                openMode: "Container"
-    //            });
-    //    }
-    //};
-    //
-    //var subscribe = function (view) {
-    //    var exchange = window.InfinniUI.global.messageBus;
-    //
-    //    exchange.subscribe(messageTypes.onViewClosing, function (message) {
-    //        if (message.view !== view) {
-    //            return;
-    //        }
-    //        view.close();
-    //    });
-    //};
 };
 
 _.extend(OpenModeContainerStrategy.prototype, {
