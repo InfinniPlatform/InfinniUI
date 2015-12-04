6930817f99286a3e021c24e360ff06467d3027d6
k.maksimov <k.maksimov@infinnitysolutions.com>
2015-12-04 16:37:02 +0500

    urlManager

--- app/utils/urlManager.js
+++ app/utils/urlManager.js
@@ -17,5 +17,55 @@ var urlManager = {
         }
 
         return result;
+    },
+
+    clearUrlSearchPath: function(){
+        var searchPath = location.search,
+            index, newUrl;
+
+        if(searchPath.length > 0){
+            index = location.href.indexOf(searchPath);
+            if(index > 0){
+                newUrl = location.href.substr(0, index);
+            }
+        }
+
+        if(newUrl){
+            history.pushState(null, null, newUrl);
+        }
+
+    },
+
+    setParameter: function(name, value){
+        var oldSearch = location.search;
+        var newSearch = _.isEmpty(oldSearch) ?
+            stringUtils.format("?{0}={1}", [name, value]) :
+            stringUtils.format("{0}&{1}={2}", [oldSearch, name, value]);
+
+        var newUrl = stringUtils.format("{0}//{1}{2}{3}{4}",[location.protocol, location.host, location.pathname, newSearch, location.hash]);
+
+        history.pushState(null, null, newUrl);
+    },
+
+    deleteParameter: function(name){
+        var params = urlManager.getParams();
+        delete params[name];
+
+        var newSearch = generateSearch(params);
+
+        var newUrl = stringUtils.format("{0}//{1}{2}{3}{4}",[location.protocol, location.host, location.pathname, newSearch, location.hash]);
+        history.pushState(null, null, newUrl);
+
+        function generateSearch(params){
+            var paramsArray = [];
+            _.mapObject(params, function(val, key){
+                var param = stringUtils.format("{0}={1}",[key, val]);
+                paramsArray.push( param );
+            });
+
+            return _.isEmpty(paramsArray) ?
+                "" :
+            "?" + paramsArray.join("&");
+        };
     }
 };
