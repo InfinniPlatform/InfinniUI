93a716acb6285b15ba6a38296b8368a868607dc1
m.molodorich <m.molodorich@infinnitysolutions.com>
2015-12-08 11:44:59 +0500

    fix style for form-control and button

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 InfinniUI.config.lang = 'ru-RU';
 InfinniUI.config.maxLengthUrl = 2048;
 InfinniUI.config.cacheMetadata = false; //boolean - enable/disable cache | milliseconds
-InfinniUI.config.serverUrl = 'http://localhost:9900';
+InfinniUI.config.serverUrl = 'http://localhost:9900';//'http://10.0.0.32:9900';
 InfinniUI.config.configId = 'PTA';
 InfinniUI.config.configName = 'Хабинет';
