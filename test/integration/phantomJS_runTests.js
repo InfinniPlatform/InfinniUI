abc3d9672569d3c61519b7fc3e887e9cc78df24d
m.sadofyev <m.sadofyev@infinnity.ru>
2015-12-11 13:15:03 +0500

    fix phantomjs script

--- test/integration/phantomJS_runTests.js
+++ test/integration/phantomJS_runTests.js
@@ -5,39 +5,42 @@ page.settings.webSecurityEnabled = false;
 
 console.log('Status: started!');
 
-page.onConsoleMessage = function(msg) {
-	console.log('CONSOLE: ' + msg);
-	if(msg === "Tests finished!"){
-		setTimeout(function() {
-			phantom.exit();
-		}, 2000);
-	}
-	if(msg.indexOf('Unhandled rejection') != -1){
-		phantom.exit();
-	}
+page.onConsoleMessage = function (msg) {
+    console.log('CONSOLE: ' + msg);
+    if (msg === "Tests finished!") {
+        setTimeout(function () {
+            phantom.exit();
+        }, 2000);
+    }
+    /* Ситуации, при которых может произойти зацикливание */
+    if (msg.indexOf('Unhandled rejection') != -1 ||
+		msg.indexOF('signOut not called') != -1
+	) {
+        phantom.exit(2);
+    }
 };
 
-page.onPageCreated = function(newPage) {
-	console.log('A new child page was created!');
-	newPage.onClosing = function(closingPage) {
-  		console.log('A child page is closing: ' + closingPage.url);
-	};
-	newPage.onConsoleMessage = function(msg) {
-		console.log('CONSOLE: ' + msg);
-	};
-	newPage.onLoadFinished = function(){
-		console.log('New child page url:' + newPage.url);
+page.onPageCreated = function (newPage) {
+    console.log('A new child page was created!');
+    newPage.onClosing = function (closingPage) {
+        console.log('A child page is closing: ' + closingPage.url);
+    };
+    newPage.onConsoleMessage = function (msg) {
+        console.log('CONSOLE: ' + msg);
+    };
+    newPage.onLoadFinished = function () {
+        console.log('New child page url:' + newPage.url);
     };
 };
 
 page.clearMemoryCache();
 
 page.open(url, function (status) {
-	
-	if(status === 'success'){
-		console.log('Status: openned!');
-	}else{
-		console.log('Status: something went wrong!');
-		phantom.exit();
-	}
+
+    if (status === 'success') {
+        console.log('Status: openned!');
+    } else {
+        console.log('Status: something went wrong!');
+        phantom.exit(1);
+    }
 });
