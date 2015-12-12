483763ebaa90cc01139c6d54b4dcc3f989158c82
m.sadofyev <m.sadofyev@infinnity.ru>
2015-12-12 15:08:19 +0500

    PC-2407: Обновить процесс прохожденеия интеграционных тестов #progress

--- test/integration/app/cucumberHTMLListener.js
+++ test/integration/app/cucumberHTMLListener.js
@@ -1,98 +1,102 @@
 function CucumberHTMLListener($root) {
-	var formatter = new CucumberHTML.DOMFormatter($root);
+    var formatter = new CucumberHTML.DOMFormatter($root);
 
-	formatter.uri('report.feature');
+    formatter.uri('report.feature');
 
-	var currentStep;
+    var currentStep;
 
-	var self = {
-	  hear: function hear(event, callback) {
-		var eventName = event.getName();
-		switch (eventName) {
-		case 'BeforeFeature':
-		  var feature = event.getPayloadItem('feature');
-		  window.cucumberCurrentFeature = feature.getName();
-		  formatter.feature({
-			keyword     : feature.getKeyword(),
-			name        : feature.getName(),
-			line        : feature.getLine(),
-			description : feature.getDescription()
-		  });
-		  console.log("##teamcity[testSuiteStarted name='" + window.cucumberCurrentFeature + "']");
-		  break;
+    var self = {
+        hear: function hear(event, callback) {
+            var eventName = event.getName();
+            switch (eventName) {
+                case 'BeforeFeature':
+                    var feature = event.getPayloadItem('feature');
+                    window.cucumberCurrentFeature = feature.getName();
+                    formatter.feature({
+                        keyword: feature.getKeyword(),
+                        name: feature.getName(),
+                        line: feature.getLine(),
+                        description: feature.getDescription()
+                    });
+                    console.log("##teamcity[testSuiteStarted name='" + window.cucumberCurrentFeature + "']");
+                    break;
 
-		case 'BeforeScenario':
-		  var scenario = event.getPayloadItem('scenario');
-		  window.cucumberCurrentScenario = scenario.getName();
-		  formatter.scenario({
-			keyword     : scenario.getKeyword(),
-			name        : scenario.getName(),
-			line        : scenario.getLine(),
-			description : scenario.getDescription()
-		  });
-		  console.log("##teamcity[testSuiteStarted name='" + window.cucumberCurrentScenario + "']");
-		  break;
+                case 'BeforeScenario':
+                    var scenario = event.getPayloadItem('scenario');
+                    window.cucumberCurrentScenario = scenario.getName();
+                    formatter.scenario({
+                        keyword: scenario.getKeyword(),
+                        name: scenario.getName(),
+                        line: scenario.getLine(),
+                        description: scenario.getDescription()
+                    });
+                    console.log("##teamcity[testSuiteStarted name='" + window.cucumberCurrentScenario + "']");
+                    break;
 
-		case 'BeforeStep':
-		  var step = event.getPayloadItem('step');
-		  self.handleAnyStep(step);
-		  window.cucumberCurrentStep = (step.getKeyword() + step.getName())
-		  .toString()
-		  .replace(/'/g, "|'")
-		  .replace(/\[/g, "|[")
-		  .replace(/\]/g, "|]")
-		  .replace(/:/g, "");
-		  console.log("##teamcity[testStarted name='" + window.cucumberCurrentStep + "']");
-		  break;
+                case 'BeforeStep':
+                    var step = event.getPayloadItem('step');
+                    self.handleAnyStep(step);
+                    window.cucumberCurrentStep = (step.getKeyword() + step.getName())
+                    .toString()
+                    .replace(/'/g, "|'")
+                    .replace(/\[/g, "|[")
+                    .replace(/\]/g, "|]")
+                    .replace(/:/g, "");
+                    console.log("##teamcity[testStarted name='" + window.cucumberCurrentStep + "']");
+                    break;
 
-		case 'StepResult':
-		var teamcityOutput = "##teamcity[";
-		  var result;
-		  var stepResult = event.getPayloadItem('stepResult');
-		  if (stepResult.isSuccessful()) {
-			result = {status: 'passed'};
-		  } else if (stepResult.isPending()) {
-			result = {status: 'pending'};
-		  } else if (stepResult.isUndefined() || stepResult.isSkipped()) {
-			result = {status:'skipped'};
-			teamcityOutput += "testIgnored name='" + window.cucumberCurrentStep + "'";
-		  } else {
-			var error = stepResult.getFailureException();
-			var errorMessage = error.stack || error;
-			result = {status: 'failed', error_message: errorMessage};
-			teamcityOutput += "testFailed name='" + window.cucumberCurrentStep + "' details='" + errorMessage.toString().replace(/\n/g, "|n|r") + "'";
-		  }
-		  formatter.match({uri:'report.feature', step: {line: currentStep.getLine()}});
-		  formatter.result(result);
-		  teamcityOutput += "]";
-		  if(teamcityOutput != "##teamcity[]"){
-			  console.log(teamcityOutput);
-		  }
-		  break;
-		  
-		  case 'AfterStep':
-			console.log("##teamcity[testFinished name='" + window.cucumberCurrentStep + "']");
-		  break;
-		  
-		  case 'AfterScenario':
-			console.log("##teamcity[testSuiteFinished name='" + window.cucumberCurrentScenario + "']");
-		  break;
+                case 'StepResult':
+                    var teamcityOutput = "##teamcity[";
+                    var result;
+                    var stepResult = event.getPayloadItem('stepResult');
+                    if (stepResult.isSuccessful()) {
+                        result = { status: 'passed' };
+                    } else if (stepResult.isPending()) {
+                        result = { status: 'pending' };
+                    } else if (stepResult.isUndefined() || stepResult.isSkipped()) {
+                        result = { status: 'skipped' };
+                        teamcityOutput += "testIgnored name='" + window.cucumberCurrentStep + "'";
+                    } else {
+                        var error = stepResult.getFailureException();
+                        var errorMessage = error.stack || error;
+                        result = { status: 'failed', error_message: errorMessage };
+                        teamcityOutput += "testFailed name='" + window.cucumberCurrentStep + "' details='" + errorMessage.toString().replace(/\n/g, "|n|r") + "'";
+                        var screenshotName =    window.cucumberCurrentFeature.toString().replace(/[?:"]*/g, "") + '.' +
+                                                window.cucumberCurrentScenario.toString().replace(/[?:"]*/g, "") + '.' +
+                                                window.cucumberCurrentStep.toString().replace(/[?:"]*/g, "");
+                        console.log('Take screenshot:' + screenshotName);
+                    }
+                    formatter.match({ uri: 'report.feature', step: { line: currentStep.getLine() } });
+                    formatter.result(result);
+                    teamcityOutput += "]";
+                    if (teamcityOutput != "##teamcity[]") {
+                        console.log(teamcityOutput);
+                    }
+                    break;
 
-		  case 'AfterFeature':
-		  	console.log("##teamcity[testSuiteFinished name='" + window.cucumberCurrentFeature + "']");
-		  break;
-		}
-		callback();
-	  },
+                case 'AfterStep':
+                    console.log("##teamcity[testFinished name='" + window.cucumberCurrentStep + "']");
+                    break;
 
-	  handleAnyStep: function handleAnyStep(step) {
-		formatter.step({
-		  keyword: step.getKeyword(),
-		  name   : step.getName(),
-		  line   : step.getLine(),
-		});
-		currentStep = step;
-	  }
-	};
-	return self;
+                case 'AfterScenario':
+                    console.log("##teamcity[testSuiteFinished name='" + window.cucumberCurrentScenario + "']");
+                    break;
+
+                case 'AfterFeature':
+                    console.log("##teamcity[testSuiteFinished name='" + window.cucumberCurrentFeature + "']");
+                    break;
+            }
+            callback();
+        },
+
+        handleAnyStep: function handleAnyStep(step) {
+            formatter.step({
+                keyword: step.getKeyword(),
+                name: step.getName(),
+                line: step.getLine(),
+            });
+            currentStep = step;
+        }
+    };
+    return self;
 };
--- test/integration/phantomJS_runTests.js
+++ test/integration/phantomJS_runTests.js
@@ -18,15 +18,24 @@ page.onConsoleMessage = function (msg) {
 	) {
         phantom.exit(2);
     }
+
+    if (msg.indexOf('Take screenshot') != -1) {
+        var fileName = msg.substring(msg.indexOf(':') + 1);
+        page.pages[0].render('Error logs/' + fileName + '.png');
+    }
 };
 
 page.onPageCreated = function (newPage) {
+    newPage.viewportSize = {
+        width: 1920,
+        height: 1080
+    };
     console.log('A new child page was created!');
     newPage.onClosing = function (closingPage) {
         console.log('A child page is closing: ' + closingPage.url);
     };
     newPage.onConsoleMessage = function (msg) {
-        console.log('CONSOLE: ' + msg);
+        console.log('CONSOLE(child page): ' + msg);
         /* Ситуации, при которых может произойти зацикливание */
         if (msg.indexOf('Unhandled rejection') != -1 ||
             msg.indexOf('signOut not called') != -1
