37a894db2f12c59223c0a3d4a731d39a4c9fcaaf
m.molodorich <m.molodorich@infinnitysolutions.com>
2015-12-16 13:24:32 +0500

    UI-1464: Property: "$" должно создавать общий Action, а не ItemAction #resolved

--- test/unit/actions/editAction.js
+++ test/unit/actions/editAction.js
@@ -1,52 +1,34 @@
-//describe('EditAction', function () {
-//    it('successful build', function () {
-//        // Given
-//        var view = new View();
-//        var builder = new ApplicationBuilder();
-//        var dataSource = new ObjectDataSource({ name: 'SomeDS', view: view });
-//
-//        dataSource.setItems([{Id:1},{Id:2}]);
-//
-//        view.getDataSources().push(dataSource);
-//
-//        var metadata = {
-//            StackPanel: {
-//                Items: {
-//                    Source: "SomeDS",
-//                    Property: ""
-//                },
-//                ItemTemplate: {
-//                    Button: {
-//                        Name: "EditButton",
-//                        Action:{
-//                            EditAction: {
-//                                LinkView: {
-//                                    InlineView: {
-//
-//                                    }
-//                                },
-//                                DestinationValue: {
-//                                    Source: 'SomeDS'
-//                                },
-//                                SourceValue: {
-//                                    Source: 'EditDS'
-//                                }
-//                            }
-//                        }
-//
-//                    }
-//                }
-//            }
-//
-//        };
-//
-//        // When
-//        var stackPanel = builder.build(metadata, {parentView: view});
-//        debugger;
-//
-//        // Then
-//        //assert.isNotNull( editAction );
-//        //assert.isNotNull( editAction.execute, 'action should have execute' );
-//    });
-//
-//});
+describe('EditAction', function () {
+    it('successful build', function () {
+        // Given
+        var view = new View();
+        var builder = new ApplicationBuilder();
+        var dataSource = new ObjectDataSource({ name: 'SomeDS', view: view });
+
+        view.getDataSources().push(dataSource);
+
+        var metadata = {
+            EditAction: {
+                LinkView: {
+                    InlineView: {
+
+                    }
+                },
+                DestinationValue: {
+                    Source: 'SomeDS'
+                },
+                SourceValue: {
+                    Source: 'EditDS'
+                }
+            }
+        };
+
+        // When
+        var editAction = builder.build(metadata, {parentView: view});
+
+        // Then
+        assert.isNotNull( editAction );
+        assert.isNotNull( editAction.execute, 'action should have execute' );
+    });
+
+});
