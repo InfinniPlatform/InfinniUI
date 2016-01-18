7860ad953f1d91d3c858e09e7aa7ed8442262776
k.maksimov <k.maksimov@infinnitysolutions.com>
2016-01-18 18:03:25 +0500

    minor

--- example/Build.bat
+++ example/Build.bat
@@ -1,9 +1,10 @@
 
 
 SET InfinniUIPath=..
+SET LauncherPath=example.
 
 pushd %InfinniUIPath%
-call grunt build:{override\:{less\:{'pl-override-platform-variables-path'\:'\"%InfinniUIPath%/../example/styles/platform-variables.less\"','pl-override-bootstrap-variables-path'\:'\"%InfinniUIPath%/../example/styles/bootstrap-variables.less\"','pl-bootstrap-theme-path'\:'\"%InfinniUIPath%/../example/styles/bootstrap-theme.less\"','pl-extension-path'\:'\"%InfinniUIPath%/../example/styles/extensions.less\"'}}}
+call grunt build:{override\:{less\:{'pl-override-platform-variables-path'\:'\"%InfinniUIPath%/../%LauncherPath%/styles/platform-variables.less\"','pl-override-bootstrap-variables-path'\:'\"%InfinniUIPath%/../%LauncherPath%/styles/bootstrap-variables.less\"','pl-bootstrap-theme-path'\:'\"%InfinniUIPath%/../%LauncherPath%/styles/bootstrap-theme.less\"','pl-extension-path'\:'\"%InfinniUIPath%/../%LauncherPath%/styles/extensions.less\"'}}}
 popd
 
 xcopy %InfinniUIPath%\out\* platform\ /s /y /r
