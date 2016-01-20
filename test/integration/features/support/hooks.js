99370a94e85b88f5b4a636b23fd6e5d0d04b0503
m.sadofyev <m.sadofyev@infinnity.ru>
2016-01-20 12:13:12 +0500

    PC-2737: Восстановить глобальные вспомогательные методы #resolved

---
+++ test/integration/features/support/hooks.js
@@ -0,0 +1,60 @@
+
+this.BeforeScenario( function(scenario, callback) {
+	var snapshotPath = window.navigator.platform.indexOf('Linux') == -1 ? "C:\\ESbackups" : "/tmp/ESbackups"
+	
+	console.log('Before scenario');
+		//delete indices, create repository and restore
+		client.indices.delete({
+			"index": "administrationcustomization,authorization,pta",
+			"waitForCompletion": true
+		}).then(function(){
+			client.snapshot.createRepository({
+				"repository": "my_backup",
+				"body":{
+					"type": "fs",
+					"settings": {
+						"compress": "true",
+						"location": snapshotPath
+					}
+				}
+			}).then(function(){
+				client.snapshot.restore({
+					"repository": "my_backup",
+					"snapshot": "snapshot1",
+					"waitForCompletion": true
+				}).then(function(){
+					window.configWindow = window.open(window.IntegrationTestConfig.host);	
+	
+					var signOut = function(){
+						window.configWindow.contextApp.context.global.session.signOut(function () {
+							window.configWindow.location.reload();
+							callback();
+						});	
+					};
+					
+					var error = function(){
+						console.log('signOut not called!');
+					}
+				
+					window.testHelpers.waitCondition(function(){
+						return window.configWindow.contextApp != null;
+					}, signOut, error);
+				});	
+			});
+		});
+});
+
+this.AfterFeatures(function(){
+	console.log("Tests finished!");
+});
+
+this.AfterScenario( function(scenario, callback) {
+	window.configWindow.close();
+	callback();
+});
+
+this.AfterStep(function(step, callback){
+	setTimeout(function() {
+		callback();
+	}, 200);
+});
