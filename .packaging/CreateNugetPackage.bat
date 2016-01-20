403e78bd7b8c77dfa2153fc4837e65645df0f4cc
k.maksimov <k.maksimov@infinnitysolutions.com>
2016-01-20 09:10:01 +0500

    move files from master

---
+++ .packaging/CreateNugetPackage.bat
@@ -0,0 +1,25 @@
+@echo off
+::Check versions
+IF [%1] == [] (
+	echo "Set UI version as parameter!"
+	EXIT /B
+)
+
+::Set package version in files
+powershell -NoProfile -ExecutionPolicy Bypass -Command ".\SetTemplateStubs.ps1 %1"
+
+pushd ..
+::Clear build artifacts
+rd /S /Q out\
+
+::Build project
+call npm install
+call bower install
+call grunt build
+popd
+
+::Create packages
+nuget Pack UI.nuspec
+
+::Clean up
+del UI.nuspec /Q /F
