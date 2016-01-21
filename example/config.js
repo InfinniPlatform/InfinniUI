5f61445a4d7f2d37694e6ec5599af13769a3e0cf
k.maksimov <k.maksimov@infinnitysolutions.com>
2016-01-21 17:42:56 +0500

    config

--- example/config.js
+++ example/config.js
@@ -4,6 +4,6 @@ window.InfinniUI.config = window.InfinniUI.config || {};
 // перекрываем дефолтные конфиги, лажащие в InfinniUI/app/config.js
 
 window.InfinniUI.config.cacheMetadata = false;
-window.InfinniUI.config.serverUrl = 'http://10.0.0.32:9900'; // Сергей, привет!
+window.InfinniUI.config.serverUrl = 'http://localhost:9900'; 
 window.InfinniUI.config.configId = 'PTA';
 window.InfinniUI.config.configName = 'Хабинет';
