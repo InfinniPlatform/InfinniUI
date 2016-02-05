5d82b35ef024a9b89381c1432f17490358c08683
k.vlasov <kostya.vlasov@gmail.com>
2016-02-05 13:57:58 +0500

    UI-1602: Визуально снимается выбор строки у DataGrid#progress

--- app/utils/hashMap.js
+++ app/utils/hashMap.js
@@ -7,10 +7,21 @@ function HashMap() {
     this._values = [];
 }
 
-Object.defineProperties(HashMap.prototype, {
+Object.defineProperties(HashMap.prototype, /** @lends HashMap.prototype **/{
     length: {
         get: function () {
             return this._keys.length;
+        }
+    },
+    keys: {
+        get: function () {
+            return this._keys;
+        }
+    },
+
+    values: {
+        get: function () {
+            return this._values;
         }
     }
 });
@@ -24,6 +35,36 @@ HashMap.prototype.add = function (key, value) {
     } else {
         this._values[i] = value;
     }
+};
+
+HashMap.prototype.getKeyByValue = function (value) {
+    var key,
+        i = this._getIndexOfValue(value);
+
+    if (i !== -1) {
+        key = this._keys[i];
+    }
+    return key;
+};
+
+/**
+ *
+ * @param {Function} predicate
+ * @param thisArg
+ * @returns {numeric}
+ */
+HashMap.prototype.findIndex = function (predicate, thisArg) {
+    var key, value, index = -1;
+    for (var i = 0; i < this._keys.length; i = i + 1) {
+        key = this._keys[i];
+        value = this._values[i];
+        if (predicate.call(thisArg, key, value)) {
+            index =  i;
+            break;
+        }
+    }
+
+    return index;
 };
 
 HashMap.prototype.get = function (key) {
@@ -58,4 +99,13 @@ HashMap.prototype.clear = function (callback) {
  */
 HashMap.prototype._getIndexOfKey = function (key) {
     return this._keys.indexOf(key);
+};
+
+/**
+ * @param {*} value
+ * @returns {number}
+ * @private
+ */
+HashMap.prototype._getIndexOfValue = function (value) {
+    return this._values.indexOf(value);
 };
