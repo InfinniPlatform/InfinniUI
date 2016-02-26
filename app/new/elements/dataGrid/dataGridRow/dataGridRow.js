c3fd8988e7626a490accedcf37c0b5cf8e1b3b69
k.vlasov <kostya.vlasov@gmail.com>
2016-02-26 18:09:36 +0500

    UI-1661: Добавить в DataGrid работу со строками#resolve

--- app/new/elements/dataGrid/dataGridRow/dataGridRow.js
+++ app/new/elements/dataGrid/dataGridRow/dataGridRow.js
@@ -1,6 +1,12 @@
 function DataGridRow() {
     _.superClass(DataGridRow, this);
 
+    this._transformRowProperties({
+        rowBackground: 'background',
+        rowForeground: 'foreground',
+        rowTextStyle: 'textStyle',
+        rowStyle: 'style'
+    });
 }
 
 _.inherit(DataGridRow, Element);
@@ -34,6 +40,60 @@ _.extend(DataGridRow.prototype, {
 
     onToggle: function (handler) {
         this.control.onToggle(handler);
+    },
+
+    /** RowBackground **/
+    setRowBackground: function (value) {
+        this.control.set('rowBackground', value);
+    },
+
+    getRowBackground: function () {
+        return this.control.get('rowBackground');
+    },
+
+    /** RowForeground **/
+    setRowForeground: function (value) {
+        this.control.set('rowForeground', value);
+    },
+
+    getRowForeground: function () {
+        return this.control.get('rowForeground');
+    },
+
+    /** RowTextStyle */
+    setRowTextStyle: function (value) {
+        this.control.set('rowTextStyle', value);
+    },
+
+    getRowTextStyle: function () {
+        return this.control.get('rowTextStyle');
+    },
+
+    /** RowStyle */
+    setRowStyle: function (value) {
+        this.control.set('rowStyle', value);
+    },
+
+    getRowStyle: function () {
+        return this.control.get('rowStyle');
+    },
+
+    _transformRowProperties: function (properties) {
+
+        for(var name in properties) {
+            if (!properties.hasOwnProperty(name)) {
+                continue;
+            }
+
+            this.setProperty(properties[name], this.getProperty(name));
+
+            this.onPropertyChanged(name, (function (row, prop) {
+                return function (context, args) {
+                    row.setProperty(prop, args.newValue);
+                }
+            })(this, properties[name]));
+        }
+
     }
 
 });
