b2002109ae44ed77e1ed919400d9f97670fc394c
m.sadofyev <m.sadofyev@infinnity.ru>
2016-03-09 16:37:04 +0500

    UI-1731: При открытии второго диалогового окна, прятать первое #resolved

---
+++ app/services/modalWindowService.js
@@ -0,0 +1,32 @@
+InfinniUI.ModalWindowService = (function () {
+    var modalQueue = [];
+
+    return {
+        modalWasOpened: function (obj) {
+            if (modalQueue.length != 0) {
+                var previous = modalQueue[modalQueue.length - 1];
+
+                previous.modal.hide();
+                previous.background.hide();
+            }
+
+            modalQueue.push(obj);
+        },
+
+        modalWasClosed: function (modal) {
+            for (var i = 0, length = modalQueue.length; i < length; i++) {
+                if (modalQueue[i].modal == modal) {
+                    // Если последний
+                    if (i == length - 1 && i != 0) {
+                        var previous = modalQueue[i - 1];
+
+                        previous.modal.show();
+                        previous.background.show();
+                    }
+
+                    modalQueue.splice(i, 1);
+                }
+            }
+        }
+    };
+})();
