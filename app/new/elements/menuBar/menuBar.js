56ca5456c0ebc8ebe9d86f94c1e156e73666fb03
k.vlasov <kostya.vlasov@gmail.com>
2016-04-19 16:37:51 +0500

    UI-1887: Подсветка выбранного раздела на панели навигации#fixed

--- app/new/elements/menuBar/menuBar.js
+++ app/new/elements/menuBar/menuBar.js
@@ -15,6 +15,10 @@ _.extend(MenuBar.prototype, {
         return new MenuBarControl(viewMode);
     },
 
+    /**
+     * @description Устанавливает/снимает выделение элемента меню с заданным именем
+     * @param {String|*} name Имя выделяемого элемента меню
+     */
     highlightItem: function (name) {
         (function highlight(element) {
             var childElements = element.getChildElements();
