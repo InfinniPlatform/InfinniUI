7012f203d47733bd520f8dfd438bbb1f34f64290
Andrey Viriyalov <a.viriyalov@infinnity.ru>
2016-06-17 11:06:10 +0500

    f-UI-2060 Тестовая конфигурация для демонстрации возможностей InfinniUI #created first variant

--- example/www/config.js
+++ example/www/config.js
@@ -4,8 +4,8 @@ window.InfinniUI.config = window.InfinniUI.config || {};
 // перекрываем дефолтные конфиги, лажащие в InfinniUI/app/config.js
 
 window.InfinniUI.config.cacheMetadata = false;
-window.InfinniUI.config.serverUrl = 'http://localhost:9900';
+window.InfinniUI.config.serverUrl = 'http://localhost:8181';
 window.InfinniUI.config.configId = 'PTA';
 window.InfinniUI.config.configName = 'Хабинет';
 
-window.InfinniUI.config.homePage = 'testConfigurations/indeterminateCheckbox.json'; //{ConfigId: InfinniUI.config.configId, DocumentId: 'Common', MetadataName: 'HomePage'}; //'stab.json'
+window.InfinniUI.config.homePage = 'testConfigurations/powerfulInfinniUI.json'; //{ConfigId: InfinniUI.config.configId, DocumentId: 'Common', MetadataName: 'HomePage'}; //'stab.json'
