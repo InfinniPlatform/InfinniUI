5e267884f836927cfa55502c0447ffb976e48ab8
m.molodorich <m.molodorich@infinnitysolutions.com>
2016-07-14 14:51:02 +0500

    remove documentViewer

--- app/builders/applicationBuilder.js
+++ app/builders/applicationBuilder.js
@@ -57,7 +57,6 @@ _.extend(ApplicationBuilder.prototype, {
         builder.register('ExtensionPanel', new ExtensionPanelBuilder());
         builder.register('PopupButton', new PopupButtonBuilder());
         builder.register('DataNavigation', new DataNavigationBuilder());
-        builder.register('DocumentViewer', new DocumentViewerBuilder());
         builder.register('PdfViewer', new PdfViewerBuilder());
         builder.register('TreeView', new TreeViewBuilder());
         builder.register('Frame', new FrameBuilder());
