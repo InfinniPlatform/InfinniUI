24e13ba5d0888d302ecf58263dcf28ce4591694e
m.sadofyev <m.sadofyev@infinnity.ru>
2016-07-26 21:16:13 +0500

    UI-2201: Конструкция "Флаг будет иметь значение" #resolved

--- test/integration/features/step_definitions/checkBox.js
+++ test/integration/features/step_definitions/checkBox.js
@@ -14,4 +14,21 @@ module.exports = function () {
                 return elements[checkBoxName.index].click();
             });
     });
+
+    this.When(/^флаг "([^"]*)" будет иметь значение "([^"]*)"$/, function (checkBoxName, value) {
+        checkBoxName = this.helpers.parseElement(checkBoxName);
+        value = JSON.parse(value);
+
+        var selector = this.selectors.XPATH.CheckBox.self(checkBoxName.name);
+        var xpath = this.by.xpath(selector);
+        var that = this;
+
+        return this.currentView.findElements(xpath)
+            .then(function (elements) {
+                return elements[checkBoxName.index].isSelected();
+            })
+            .then(function (actualValue) {
+                that.assert.equal(actualValue, value);
+            });
+    });
 };
