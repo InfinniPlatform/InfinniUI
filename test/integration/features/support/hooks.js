e7e4e92749ae9f974df8f7d0df3d280bf14500d5
m.sadofyev <m.sadofyev@infinnity.ru>
2016-08-10 13:47:20 +0500

    Ожидание окончания работы лоадера

--- test/integration/features/support/hooks.js
+++ test/integration/features/support/hooks.js
@@ -1,19 +1,21 @@
 'use strict';
 
 var driver = require('./world.js').getDriver();
+var by = require('./world.js').By;
 var fs = require('fs');
 var path = require('path');
 var sanitize = require('sanitize-filename');
 var args = require('../../helpers/arguments.js')(process.argv.slice(2));
+var config = require('./config.json');
 
 var myHooks = function () {
-  
-  this.After(function(scenario) {
-    if(scenario.isFailed()) {
-      this.driver.takeScreenshot().then(function(data){
-        var base64Data = data.replace(/^data:image\/png;base64,/,"");
-        fs.writeFile(path.join('screenshots', sanitize(scenario.getName() + ".png").replace(/ /g,"_")), base64Data, 'base64', function(err) {
-            if(err) console.log(err);
+
+  this.After(function (scenario) {
+    if (scenario.isFailed()) {
+      this.driver.takeScreenshot().then(function (data) {
+        var base64Data = data.replace(/^data:image\/png;base64,/, "");
+        fs.writeFile(path.join('screenshots', sanitize(scenario.getName() + ".png").replace(/ /g, "_")), base64Data, 'base64', function (err) {
+          if (err) console.log(err);
         });
       });
     }
@@ -22,6 +24,31 @@ var myHooks = function () {
 
   this.registerHandler('AfterFeatures', function (event) {
     return driver.quit();
+  });
+
+  this.registerHandler('BeforeStep', function (step, callback) {
+    var attempt = 0;
+    var totalAttempts = 5;
+
+    driver.manage().timeouts().implicitlyWait(config.timeouts.wait);
+
+    (function tryContinue(i) {
+      driver.findElements(by.xpath('.//div[contains(@class, "blockPage")]'))
+          .then(function (elements) {
+            if (!elements.length) {
+              driver.manage().timeouts().implicitlyWait(config.timeouts.main);
+              callback();
+            } else {
+              if (i < totalAttempts) {
+                setTimeout(function () {
+                  tryContinue(++i);
+                }, 1000);
+              } else {
+                throw new Error('Try again');
+              }
+            }
+          });
+    })(1);
   });
 
   try {
