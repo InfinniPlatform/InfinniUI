23866d42f9977582044eb6bf791f0c2db56634b5
m.molodorich <m.molodorich@infinnitysolutions.com>
2016-08-11 15:47:38 +0500

    UI-2291: Отрефакторить unit тесты ExtensionPanel#resolved

--- test/unit/elements/extensionPanel/extensionPanelBuilder.js
+++ test/unit/elements/extensionPanel/extensionPanelBuilder.js
@@ -1,24 +1,39 @@
-//describe('Extension Panel', function () {
-//    it('should be true if scriptsHandlers call', function () {
-//        //Given
-//        var extensionPanel = new ApplicationBuilder();
-//        var view = new View();
-//        var metadata = {
-//            ExtensionPanel: {
-//                ExtensionName: 'Banner',
-//                OnLoaded: {
-//                    Name: 'OnLoaded'
-//                }
-//            }
-//        };
-//        window.Test = {extensionPanelLoaded:false};
-//        view.setScripts([{Name:"OnLoaded", Body:"window.Test.extensionPanelLoaded = true"}]);
-//
-//        //When
-//        var build = extensionPanel.build(view, metadata);
-//        var el = build.render();
-//
-//        // Then
-////        assert.isTrue(window.Test.extensionPanelLoaded);
-//    });
-//});
+describe('Extension Panel (build)', function () {
+    it('successful build', function () {
+        // Given
+        var builder = new ExtensionPanelBuilder();
+        var metadata = {};
+
+        // When
+        var extensionPanel = builder.build(null, {builder: new ApplicationBuilder(), metadata: metadata, parentView: new View()});
+
+        // Then
+        assert.isNotNull(extensionPanel);
+    });
+
+    it('should find extensionPanel in global namespace', function (done) {
+        // Given
+        var builder = new ExtensionPanelBuilder();
+        var metadata = {
+            "ExtensionName": "myExtensionPanel",
+            "Parameters": [
+                {
+                    "Name": "param1",
+                    "Value": "hello"
+                }
+            ]
+        };
+        var extensionPanel = builder.build(null, {builder: new ApplicationBuilder(), metadata: metadata, parentView: new View()});
+
+        window.myExtensionPanel = function(context, args){
+            // Then
+            assert.isDefined(args.$el);
+            assert.isDefined(args.parameters);
+
+            this.render = done;
+        };
+
+        // When
+        extensionPanel.render();
+    });
+});
