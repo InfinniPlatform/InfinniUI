a907bd6a260029950cdf509513b6d53acdb86a8e
m.sadofyev <m.sadofyev@infinnity.ru>
2016-08-23 12:26:19 +0500

    Обернуть код плафтормы в функцию в platform.js

--- gulpfile.js/gulptasks/concatJs.js
+++ gulpfile.js/gulptasks/concatJs.js
@@ -1,22 +1,26 @@
 'use strict';
 
 var gulp = require('gulp'),
-			$ = require('gulp-load-plugins')(),
-			combiner = require('stream-combiner2').obj;
+    $ = require('gulp-load-plugins')(),
+    combiner = require('stream-combiner2').obj;
 
-module.exports = function(options) {
-	return function(callback) {
-		return combiner(
-			gulp.src(options.src, {base: '.'}),
-			$.wrapper({
-				header: function(file) {
-					return '//####' + file.relative + '\n';
-				}
-			}),
-			$.concat(options.finalName),
-			gulp.dest(options.dest)
-		).on('error', $.notify.onError({
-				title: options.taskName
-		}));
-	};
+module.exports = function (options) {
+    return function (callback) {
+        return combiner(
+            gulp.src(options.src, {base: '.'}),
+            $.wrapper({
+                header: function (file) {
+                    return '//####' + file.relative + '\n';
+                }
+            }),
+            $.concat(options.finalName),
+            $.wrapper({
+                header: ';(function(){\n',
+                footer: '})();'
+            }),
+            gulp.dest(options.dest)
+        ).on('error', $.notify.onError({
+            title: options.taskName
+        }));
+    };
 };
