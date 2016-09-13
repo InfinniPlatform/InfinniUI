17c328d3c602dedbfdd201d07e35522ed7cb0cf7
m.molodorich <m.molodorich@infinnitysolutions.com>
2016-09-13 11:26:50 +0500

    UI-2385:  ScriptExecutor #resolved

--- test/unit/script/scriptExecutor.js
+++ test/unit/script/scriptExecutor.js
@@ -1,30 +1,57 @@
 describe('ScriptExecutor', function () {
-    var builder;
-    beforeEach(function () {
-        builder = new InfinniUI.ScriptBuilder();
-    });
 
     it('should build script handler', function () {
-
+        // Given
+        var builder = new InfinniUI.ScriptBuilder();
         var metadata = {
             Name: "Name",
             Body: "return 5;"
         };
 
+        // When
         var func = builder.build(null, {metadata: metadata});
 
+        // Then
         assert.equal(func.call(), 5);
     });
 
     it('should pass arguments to handler', function () {
-
+        // Given
+        var builder = new InfinniUI.ScriptBuilder();
         var metadata = {
             Name: "Name",
             Body: "return [context,args].join(':');"
         };
 
+        // When
         var func = builder.build(null, {metadata: metadata});
 
+        // Then
         assert.equal(func.call(undefined, "Context", "Args"), "Context:Args");
+    });
+
+    it('should execute script by name', function (done) {
+        var metadata = {
+            Scripts: [
+                {
+                    Name: "TestScript",
+                    Body: "window.testScriptWasExecuted = true;"
+                }
+            ]
+        };
+
+        var onViewReady = function(view) {
+            // Given
+            window.testScriptWasExecuted = false;
+
+            //When
+            new window.InfinniUI.ScriptExecutor(view).executeScript('TestScript');
+
+            //Then
+            assert.isTrue(window.testScriptWasExecuted);
+            done();
+        };
+
+        testHelper.applyViewMetadata(metadata, onViewReady);
     });
 });
