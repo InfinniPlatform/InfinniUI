1df8bc1049f77e0a5739f39f0273a071bc704241
m.molodorich <m.molodorich@infinnitysolutions.com>
2016-09-13 11:26:21 +0500

    UI-2385:  ScriptExecutor #progress

--- test/unit/script/scriptExecutor.js
+++ test/unit/script/scriptExecutor.js
@@ -1,146 +1,30 @@
-//describe('ScriptExecutor', function () {
-//
-//    var builder = new InfinniUI.ApplicationBuilder();
-//
-//    window.InfinniUI.providerRegister.register('MetadataDataSource', function (metadataValue) {
-//        return {
-//            "getMetadata": function () {
-//                return metadata;
-//            }
-//        };
-//    });
-//
-//    var metadata = {
-//        "Scripts" :[
-//            {
-//                "Name" : "OpenViewScript",
-//                "Body" : "context.Controls['TextBox1'].setText('Hello world from script!');"
-//            },
-//            {
-//                "Name" : "TestRunScript",
-//                "Body" : "context.TestValue = args['test'];"
-//            }
-//        ],
-//        "DataSources": [
-//
-//            {
-//                "DocumentDataSource": {
-//                    "Name": 'PatientDataSource',
-//                    "ConfigId": 'ReceivingRoom',
-//                    "DocumentId": 'HospitalizationRefusal'
-//                }
-//            },
-//            {
-//                "DocumentDataSource" : {
-//                    "Name" : 'ClassifierDataSource',
-//                    "ConfigId" : 'ClassifierStorage',
-//                    "DocumentId" : 'SomeClassifier'
-//                }
-//            }
-//        ],
-//        "OpenMode": "Application",
-//        "ViewType": 'ListView',
-//        //"Parameters": [
-//        //    {
-//        //        "Name" : 'Param1'
-//        //    }
-//        //],
-//        "LayoutPanel" : {
-//            "StackPanel": {
-//                "Name": "MainViewPanel",
-//                "Items": [
-//                    {
-//                        "TextBox": {
-//                            "Name": "TextBox1",
-//                            "Multiline": true
-//                        }
-//                    },
-//                    {
-//                        "TextBox": {
-//                            "Name": "TextBox2"
-//                        }
-//                    },
-//                    {
-//                        "ComboBox": {
-//                            "Name": "ComboBox1",
-//                            "DisplayProperty": "",
-//                            "ValueProperty": "",
-//                            "MultiSelect": true,
-//                            "ShowClear": true,
-//                            "Value" : {
-//                                "DataSource" : "PatientDataSource",
-//                                "Property" : "LastName"
-//                            }
-//                        }
-//                    }
-//                ]
-//            }
-//        }
-//    };
-//
-////    it('should create script context for opened view', function (done) {
-////
-////        var linkView = new InfinniUI.LinkView(null, function (resultCallback) {
-////            var builder = new InfinniUI.ApplicationBuilder();
-////            var view = builder.buildType(fakeView(), 'View', metadata);
-////            resultCallback(view);
-////        });
-////        linkView.setOpenMode('Application');
-////
-////        linkView.createView(function(view){
-////
-////            assert.isNotNull(view.getContext());
-////            assert.isNotNull(view.getContext().DataSources['PatientDataSource']);
-////            assert.isNotNull(view.getContext().DataSources['ClassifierDataSource']);
-////            //assert.isNotNull(view.getContext().Parameters['Param1']);
-////            //assert.equal(view.getContext().Parameters['Param1'].getValue(),'1');
-////            assert.isNotNull(view.getContext().Controls['TextBox1']);
-////            assert.isNotNull(view.getContext().Controls['TextBox2']);
-////            assert.isNotNull(view.getContext().Controls['ComboBox1']);
-////
-////            var textBox1 = view.getContext().Controls['TextBox1'];
-////
-////            textBox1.setText('Hello world!');
-////            assert.equal(textBox1.getText(),'Hello world!');
-////
-////            var dataSource = view.getContext().DataSources["PatientDataSource"];
-////            assert.equal(dataSource.getName(),"PatientDataSource");
-////
-////            done();
-////        });
-////    });
-////
-////    it('should invoke script from ScriptExecutor', function(done){
-////
-////        var linkView = new InfinniUI.LinkView(null, function (resultCallback) {
-////            var builder = new InfinniUI.ApplicationBuilder();
-////            var view = builder.buildType(fakeView(), 'View', metadata);
-////            resultCallback(view);
-////        });
-////        linkView.setOpenMode('Application');
-////
-////        linkView.createView(function(view){
-////
-////            var scriptExecutor = new ScriptExecutor(view);
-////            scriptExecutor.executeScript('OpenViewScript');
-////
-////            var textBox1 = view.getContext().Controls["TextBox1"];
-////            assert.equal(textBox1.getText(),'Hello world from script!');
-////
-////            var args = {
-////                "test" : 1
-////            };
-////
-////            var context = view.getContext();
-////            context.Scripts["TestRunScript"].Run(context,args);
-////
-////            assert.equal(context.TestValue,1);
-////
-////            done();
-////
-////        });
-////
-////    });
-//
-//
-//});
+describe('ScriptExecutor', function () {
+    var builder;
+    beforeEach(function () {
+        builder = new InfinniUI.ScriptBuilder();
+    });
+
+    it('should build script handler', function () {
+
+        var metadata = {
+            Name: "Name",
+            Body: "return 5;"
+        };
+
+        var func = builder.build(null, {metadata: metadata});
+
+        assert.equal(func.call(), 5);
+    });
+
+    it('should pass arguments to handler', function () {
+
+        var metadata = {
+            Name: "Name",
+            Body: "return [context,args].join(':');"
+        };
+
+        var func = builder.build(null, {metadata: metadata});
+
+        assert.equal(func.call(undefined, "Context", "Args"), "Context:Args");
+    });
+});
