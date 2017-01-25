87be224ea902b5c9b58a4059f081408eca7daafd
Andrey Viriyalov <a.viriyalov@infinnity.ru>
2017-01-25 17:17:22 +0500

    UI-2516 Не работает Enabled для ListBox #resolved

--- app/elements/listBox/listBox.js
+++ app/elements/listBox/listBox.js
@@ -9,14 +9,3 @@ _.inherit(ListBox, ListEditorBase);
 ListBox.prototype.createControl = function (viewMode) {
 	return new ListBoxControl(viewMode);
 };
-
-ListBox.prototype.setEnabled = function (value) {
-	if( _.isBoolean(value) ) {
-		ListEditorBase.prototype.setEnabled.call(this, value);
-		if( value ) {
-			this.control.updateDisabledItem();
-		} else {
-			this.control.disableAll();
-		}
-	}
-};
