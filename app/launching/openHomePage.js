fe95b4fb257d19ebb2f3ddaf18951eea8ce796ce
m.molodorich <m.molodorich@infinnitysolutions.com>
2017-01-26 14:26:07 +0500

    Revert "move authentication to openHomePage"
    This reverts commit f825c6ca4a6d029151adf06e13974644524c256a.

--- app/launching/openHomePage.js
+++ app/launching/openHomePage.js
@@ -12,10 +12,6 @@ window.InfinniUI.openHomePage = function($target) {
             var action = builder.buildType('OpenAction', viewMetadata, {parentView: rootView});
             action.execute();
         });
-
-    if( InfinniUI.config.enableGetCurrentUser ) {
-        setCurrentUser();
-    }
 };
 
 function subscribeRecalculationOnWindowResize($container) {
@@ -64,26 +60,3 @@ function getHomePageLinkViewPromise() {
 
     return defer.promise();
 };
-
-function refreshUserInfo() {
-    var authProvider = InfinniUI.global.session;
-    authProvider.getCurrentUser(
-        function (result) {
-            InfinniUI.user.onReadyDeferred.resolve(result);
-        },
-        function (error) {
-            InfinniUI.user.onReadyDeferred.resolve(null);
-        }
-    );
-};
-
-function setCurrentUser() {
-    InfinniUI.user = {
-        onReadyDeferred: $.Deferred(),
-        onReady: function(handler){
-            this.onReadyDeferred.done(handler);
-        }
-    };
-
-    refreshUserInfo();
-}
