c62acde4c903c48608e96bdb30204e745e17ee15
Andrey Viriyalov <a.viriyalov@infinnity.ru>
2017-01-31 12:22:16 +0500

    UI-2621 SaveAction с параметром CanClose: false не обновляет DestinationValue #resolved

--- app/actions/saveAction/saveAction.js
+++ app/actions/saveAction/saveAction.js
@@ -1,45 +1,44 @@
-function SaveAction(parentView){
-    _.superClass(SaveAction, this, parentView);
+function SaveAction( parentView ) {
+	_.superClass( SaveAction, this, parentView );
 }
 
-_.inherit(SaveAction, BaseAction);
+_.inherit( SaveAction, BaseAction );
 
+_.extend( SaveAction.prototype,
+	BaseFallibleActionMixin,
+	{
+		execute: function( callback ) {
+			var parentView = this.parentView;
+			var dataSource = this.getProperty( 'dataSource' );
+			var canClose = this.getProperty( 'canClose' );
+			var that = this;
 
-_.extend(SaveAction.prototype,
-    BaseFallibleActionMixin,
-    {
-        execute: function(callback){
-            var parentView = this.parentView,
-                dataSource = this.getProperty('dataSource'),
-                canClose = this.getProperty('canClose'),
-                that = this;
+			var onSuccessSave = function( context, args ) {
+				parentView.setDialogResult( DialogResult.accepted );
+				if( canClose !== false ) {
+					parentView.close();
+				}
 
-            var onSuccessSave = function(context, args){
-                    if(canClose !== false){
-                        parentView.setDialogResult(DialogResult.accepted);
-                        parentView.close();
-                    }
+				that.onExecutedHandler( args );
+				that.onSuccessHandler( args );
 
-                    that.onExecutedHandler(args);
-                    that.onSuccessHandler(args);
+				if( _.isFunction( callback ) ) {
+					callback( context, args );
+				}
+			};
+			var onErrorSave = function( context, args ) {
+				that.onExecutedHandler( args );
+				that.onErrorHandler( args );
 
-                    if(_.isFunction(callback)){
-                        callback(context, args);
-                    }
-                },
-                onErrorSave = function(context, args){
-                    that.onExecutedHandler(args);
-                    that.onErrorHandler(args);
+				if( _.isFunction( callback ) ) {
+					callback( context, args );
+				}
+			};
 
-                    if (_.isFunction(callback)) {
-                        callback(context, args);
-                    }
-                };
-
-            var selectedItem = dataSource.getSelectedItem();
-            dataSource.saveItem(selectedItem, onSuccessSave, onErrorSave);
-        }
-    }
+			var selectedItem = dataSource.getSelectedItem();
+			dataSource.saveItem( selectedItem, onSuccessSave, onErrorSave );
+		}
+	}
 );
 
 window.InfinniUI.SaveAction = SaveAction;
