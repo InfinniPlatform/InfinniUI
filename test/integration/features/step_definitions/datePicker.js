111f6a9de6658983f0143887183767e42d357ff8
m.sadofyev <m.sadofyev@infinnity.ru>
2017-03-01 13:32:24 +0500

    UI-2673 Добавить возможность обращаться к полю типа дата по индексу
    #resolved

--- test/integration/features/step_definitions/datePicker.js
+++ test/integration/features/step_definitions/datePicker.js
@@ -1,62 +1,77 @@
 'use strict';
 
-module.exports = function () {
+module.exports = function() {
     this.World = require('../support/world.js').World;
 
-    this.When(/^я введу в поле типа дата "([^"]*)" значение "([^"]*)"$/, function (pickerText, date) {
-        var selector = this.selectors.XPATH.DatePicker.caption(pickerText);
+    this.When(/^я введу в поле типа дата "([^"]*)" значение "([^"]*)"$/, function(pickerText, date) {
+        var picker = this.helpers.parseElement(pickerText);
+
+        var selector = this.selectors.XPATH.DatePicker.caption(picker.name);
         var xpath = this.by.xpath(selector);
         var that = this;
 
         date = this.helpers.parseDate(date).split('.');
 
-        return this.currentView.findElement(xpath)
-            .then(function (label) {
-                return label.getAttribute('for');
+        return this.currentView.findElements(xpath)
+            .then(function(labels) {
+                if (labels.length <= picker.index) {
+                    throw new Error('Элемент не найден');
+                }
+                return labels[picker.index].getAttribute('for');
             })
-            .then(function (id) {
+            .then(function(id) {
                 return that.currentView.findElement(that.by.id(id));
             })
-            .then(function (input) {
+            .then(function(input) {
                 return input.sendKeys(that.selectAll, that.keys.BACK_SPACE, date);
             });
     });
 
-    this.When(/^значение в поле типа дата "([^"]*)" равно "([^"]*)"$/, function (pickerText, date) {
-        var selector = this.selectors.XPATH.DatePicker.caption(pickerText);
+    this.When(/^значение в поле типа дата "([^"]*)" равно "([^"]*)"$/, function(pickerText, date) {
+        var picker = this.helpers.parseElement(pickerText);
+
+        var selector = this.selectors.XPATH.DatePicker.caption(picker.name);
         var xpath = this.by.xpath(selector);
         var that = this;
 
         date = this.helpers.parseDate(date);
 
         return this.currentView.findElement(xpath)
-            .then(function (label) {
-                return label.getAttribute('for');
+            .then(function(labels) {
+                if (labels.length <= picker.index) {
+                    throw new Error('Элемент не найден');
+                }
+                return labels[picker.index].getAttribute('for');
             })
-            .then(function (id) {
+            .then(function(id) {
                 return that.currentView.findElement(that.by.id(id));
             })
-            .then(function (input) {
+            .then(function(input) {
                 return input.getAttribute('value');
             })
-            .then(function (value) {
+            .then(function(value) {
                 that.assert.equal(value, date);
             });
     });
 
-    this.When(/^я очищу поле типа дата "([^"]*)"$/, function (datePickerLabel) {
-        var selector = this.selectors.XPATH.DatePicker.caption(datePickerLabel);
+    this.When(/^я очищу поле типа дата "([^"]*)"$/, function(datePickerLabel) {
+        var picker = this.helpers.parseElement(pickerText);
+
+        var selector = this.selectors.XPATH.DatePicker.caption(picker.name);
         var xpath = this.by.xpath(selector);
         var that = this;
 
         return this.currentView.findElement(xpath)
-            .then(function (label) {
-                return label.getAttribute('for');
+            .then(function(labels) {
+                if (labels.length <= picker.index) {
+                    throw new Error('Элемент не найден');
+                }
+                return labels[picker.index].getAttribute('for');
             })
-            .then(function (id) {
+            .then(function(id) {
                 return that.currentView.findElement(that.by.id(id));
             })
-            .then(function (input) {
+            .then(function(input) {
                 return input.sendKeys(that.selectAll, that.keys.BACK_SPACE);
             });
     });
